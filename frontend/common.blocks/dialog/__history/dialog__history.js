d11442b7366476a47afe60616bbd18851fe5c4ce
Alexey <choudalex@gmail.com>
2015-09-19 20:18:55 +0300

    feat: #15 тестирование соеднения с RTM Slack API

--- frontend/common.blocks/dialog/__history/dialog__history.js
+++ frontend/common.blocks/dialog/__history/dialog__history.js
@@ -1,16 +1,34 @@
 modules.define(
     'dialog__history',
-    ['i-bem__dom', 'BEMHTML', 'socket-io'],
-    function (provide, BEMDOM, BEMHTML, io) {
+    ['i-bem__dom', 'BEMHTML', 'socket-io', 'jquery'],
+    function (provide, BEMDOM, BEMHTML, io, $) {
         provide(BEMDOM.decl(this.name, {
             onSetMod : {
                 'js' : {
                     'inited' : function () {
                         console.log('feed inited io: ', io);
                         var _this = this;
+                        var socketSlack;
 
-                        io.socket.get('/message', function () {
-                            console.log('/message/find/1 response123: ', arguments);
+                        /* Тестирование Client-side. Не трогайте :) */
+                        $.get('/csrfToken').success(function (data) {
+                            var csrfToken = data._csrf;
+                            io.socket.post('/slack/rtm.start', { _csrf : csrfToken }, function (data, jwres) {
+                                socketSlack = io(data.url);
+                                socketSlack.on('hello', function (response) {
+                                    console.log('newMessage response: ', response);
+                                    var data = response.data;
+
+                                    BEMDOM.append(_this.domElem,
+                                        BEMHTML.apply([
+                                            {
+                                                block : 'message',
+                                                content : data.message.username + ': ' + data.message.text
+                                            }
+                                        ])
+                                    );
+                                });
+                            });
                         });
 
                         io.socket.on('chat.postMessage', function (response) {
