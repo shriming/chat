21528d9663ef7c0488c3113044c051fdc99350be
Denis Borodin <BorodinDK@gmail.com>
2015-09-20 23:18:12 +0300

    feat: доверстал layout

---
+++ frontend/common.blocks/header/header.deps.js
@@ -0,0 +1,10 @@
+({
+    shouldDeps : [
+        {
+            elems : ['title', 'user']
+        },
+        {
+            block : 'logo', mods : { white : true }
+        }
+    ]
+});
--- frontend/common.blocks/page/page.deps.js
+++ frontend/common.blocks/page/page.deps.js
@@ -1,13 +1,15 @@
 ({
-    shouldDeps : [
+    mustDeps : [
         { block : 'libs' },
+        { block : 'variables' }
+    ],
+    shouldDeps : [
         { block : 'controller' },
         { block : 'clearfix' },
         { block : 'header' },
         { block : 'main' },
         { block : 'list', elems : ['item'] },
-        { block : 'variables' },
         { block : 'dialog', elems : ['header', 'history', 'console'] },
         { block : 'link' }
-    ]
+    ],
 });
